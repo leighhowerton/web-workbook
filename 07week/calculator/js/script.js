eb3f7ef99cfb884cccb49590825b09ace6b0281a
Leigh Howerton <leighhowerton@yahoo.com>
2018-09-09 09:27:55 -0500

    finished project

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,39 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function flipInteger() {
+  let flipped = eval(document.querySelector("#results").value) * -1
+document.querySelector("#results").value = flipped ;
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
