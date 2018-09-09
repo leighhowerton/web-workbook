65450e47d4563cad249fdb7aeca82ef4b63166dc
Leigh Howerton <leighhowerton@yahoo.com>
2018-09-09 08:59:14 -0500

    added

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,12 +2,28 @@
 
 $(document).ready(function() {
 
-  $('[data-row]').click(function() {
-    if ($block) {
-      $(this).append($block);
-      $block = null;
-    } else {
-      $block = $(this).children().last().detach();
+  $('[data-block]').draggable({
+    revert: 'invalid'
+  });
+
+  $('[data-stack]').droppable({
+    drop: function(event, ui){
+      let $last =
+      ($(this).children().last()).data('block');
+      // console.log(`last element on stack ${$last}`);
+      let $dragging = $(ui.draggable).data('block');
+      // console.log(`dragging element ${$dragging}`);
+      if ($dragging > $last){
+        $(ui.draggable).draggable('option ', 'revert', true);
+      }else{
+    $(ui.draggable).appendTo(this).attr('style',
+    'position: "relative"');
     }
-  })
+  }
 });
+
+function checkWin(){
+  if($("[data-stack=3], [data-stack=2]")
+.children())
+
+}
